d6441fd90aacffdac144596cf44533432893f723
Moin Palekar <moin@Moins-MacBook-Air.local>
2026-04-09 07:44:38 -0700

    feat: add Blog schema and POST/GET /blogs routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./db');
+const blogRoutes = require('./routes/blogs');
 
 const app = express();
 const PORT = 8000;
@@ -12,6 +13,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'Backend is running!' });
 });
+
+app.use('/blogs', blogRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
